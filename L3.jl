ea1937fd15a29e2d50a3ef86a2ba138eaa3d8861
akabaoglu <abdullah.kabaoglu@yahoo.com>
2023-06-23 19:42:50 +0300

    Lesson 3

--- L3.jl
+++ L3.jl
@@ -1,8 +1,8 @@
-using Plots, Optim
+using Plots
 
 function fx(x)
     x > 5 ? x^2 - 4*x + 76 : x < 5 ? x^(2)*6 : -20
 end
 
 fx(-14)
-plot(fx, -10, 14, legend = :none, c = :purple, marker = :point, ms = 2)
+plot(fx, -10, 14, legend = :none, c = :purple, marker = :dot, ms = 2)
