1c8946e403e8aa3c9d3f56952dfb91616e660d46
akabaoglu <abdullah.kabaoglu@yahoo.com>
2023-06-30 08:09:14 +0300

    Lesson 14

---
+++ L14.jl
@@ -0,0 +1,13 @@
+using Statistics, StatsBase, Plots
+
+roster = ["Seung", "Jane", "Hyeonseo", "Zach", "Paul", "Qiaoyi", "Rahnuma", "Alesha", "Abdullah"]
+
+reshape(sample(roster, 9, replace = false), 3, 3)
+
+groups = [reshape(sample(roster, 9, replace = false), 3, 3) for _ in 1:10^3]
+
+locations = [indexin(["Paul", "Zach"], groups[i]) for i in 1:10^3]
+
+sum([locations[i][1][2] == locations[i][2][2] for i in 1:10^3])
+
+
